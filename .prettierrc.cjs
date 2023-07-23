c7bac387ac46be60e603c79d76ac42e1a81f2d47
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-07-23 14:15:48 +0900

    リポジトリの初期化

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 75,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSameLine: false,
+};
